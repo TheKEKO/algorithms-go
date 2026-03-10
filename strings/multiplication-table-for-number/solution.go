cf7b6ad3a9a4918e9c5f4cf559e1c67316c69475
TheKEKO <a7214370@gmail.com>
2026-03-10 09:24:41 +0300

    feat: add multiplication_table_for_number solution (strings)

--- strings/multiplication-table-for-number/solution.go
+++ strings/multiplication-table-for-number/solution.go
@@ -1,7 +1,17 @@
 // https://www.codewars.com/kata/5a2fd38b55519ed98f0000ce/train/go
 package multiplication_table_for_number
 
+import "fmt"
+
 func MultiTable(number int) string {
-	//good luck
-	return ""
+	result := ""
+	for i := 1; i <= 10; i++ {
+		sum := number * i
+		if i == 10 {
+			result += fmt.Sprintf("%d * %d = %d", i, number, sum)
+		} else {
+			result += fmt.Sprintf("%d * %d = %d\n", i, number, sum)
+		}
+	}
+	return result
 }
