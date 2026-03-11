c8256f015d46bac16ea8f51dc61427ff28fe0e07
thekeko <a7214370@gmail.com>
2026-03-11 20:12:48 +0300

    feat: add сonvert_string_to_camel_case solution (strings)

--- strings/сonvert-string-to-camel-case/solution.go
+++ strings/сonvert-string-to-camel-case/solution.go
@@ -1,7 +1,24 @@
 // https://www.codewars.com/kata/517abf86da9663f1d2000003/train/go
 package сonvert_string_to_camel_case
 
-func ToCamelCase(s string) string {
+import (
+	"unicode"
+)
 
-	return ""
+func ToCamelCase(s string) string {
+	makeUpper := false
+	result := ""
+	for _, r := range s {
+		if r == '-' || r == '_' {
+			makeUpper = true
+			continue
+		}
+		if makeUpper {
+			r = unicode.ToUpper(r)
+			makeUpper = false
+		}
+		result += string(r)
+	}
+
+	return result
 }
