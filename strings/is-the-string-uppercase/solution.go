673a6a36057424682860b7e1c05a8f2106252ad1
thekeko <a7214370@gmail.com>
2026-03-13 17:58:55 +0300

    wip: start is_the_string_uppercase problem

---
+++ strings/is-the-string-uppercase/solution.go
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/train/go
+package Is_the_string_uppercase
+
+type MyString string
+
+func (s MyString) IsUpperCase() bool {
+	// Your code here!
+
+}
