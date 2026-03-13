f09c49d9f3b91dd016339d8e684af5013ad2ed80
thekeko <a7214370@gmail.com>
2026-03-13 18:16:11 +0300

    feat: add is_the_string_uppercase solution (strings)

--- strings/is-the-string-uppercase/solution.go
+++ strings/is-the-string-uppercase/solution.go
@@ -1,9 +1,15 @@
 // https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/train/go
-package Is_the_string_uppercase
+package is_the_string_uppercase
+
+import "unicode"
 
 type MyString string
 
 func (s MyString) IsUpperCase() bool {
-	// Your code here!
-
+	for _, v := range s {
+		if unicode.IsLetter(v) && !unicode.IsUpper(v) {
+			return false
+		}
+	}
+	return true
 }
