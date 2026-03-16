57cbf8a1ee3e5bcfe1f79a10318d928b3ec5b49b
TheKEKO <a7214370@gmail.com>
2026-03-16 22:49:39 +0300

    feat: add compare_within_margin solution (math)

--- math/compare-within-margin/solution.go
+++ math/compare-within-margin/solution.go
@@ -1,6 +1,16 @@
 // https://www.codewars.com/kata/56453a12fcee9a6c4700009c/train/go
 package compare_within_margin
 
+import (
+	"math"
+)
+
 func CloseCompare(a, b, margin float64) int {
-	return 0
+	if math.Abs(a-b) <= margin {
+		return 0
+	}
+	if a < b {
+		return -1
+	}
+	return 1
 }
