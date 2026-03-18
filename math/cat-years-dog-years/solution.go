0e6cd8e08a19a22de4ee4c72a3fe98218974e3cd
TheKEKO <a7214370@gmail.com>
2026-03-18 21:45:24 +0300

    wip: start cat_years_dog_years problem

---
+++ math/cat-years-dog-years/solution.go
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/train/go
+package cat_years_dog_years
+
+func CalculateYears(years int) (result [3]int) {
+	// Write your solution here
+	return
+}
