410c1c9030fdd484e63acf0006ed275ef8a54ec6
TheKEKO <a7214370@gmail.com>
2026-03-21 11:23:17 +0300

    feat: add Count of positives / sum of negatives solution (arrays)

--- arrays/сount-of-positives-sum-of-negatives/solution.go
+++ arrays/сount-of-positives-sum-of-negatives/solution.go
@@ -3,6 +3,14 @@
 package сount_of_positives_sum_of_negatives
 
 func CountPositivesSumNegatives(numbers []int) []int {
-	var res []int
-	return res // your code here
+	pos := 0
+	neg := 0
+	for _, v := range numbers {
+		if v > 0 {
+			pos++
+		} else if v < 0 {
+			neg += v
+		}
+	}
+	return []int{pos, neg}
 }
