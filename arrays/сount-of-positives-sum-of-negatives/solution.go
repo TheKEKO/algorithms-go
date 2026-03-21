7446b8d7b4b0b87d1db5ef7ca421b57f8de5b45a
TheKEKO <a7214370@gmail.com>
2026-03-21 11:21:53 +0300

    wip: start Count of positives / sum of negatives problem

---
+++ arrays/сount-of-positives-sum-of-negatives/solution.go
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/go
+// Count of positives / sum of negatives
+package сount_of_positives_sum_of_negatives
+
+func CountPositivesSumNegatives(numbers []int) []int {
+	var res []int
+	return res // your code here
+}
