f5ccc2dfd22b6b75a14c60c3c58d28d7ee05b48f
TheKEKO <a7214370@gmail.com>
2026-03-23 22:12:42 +0300

    feat: add The Feast of Many Beasts solution (strings)

--- strings/the-feast-of-many-beasts/solution.go
+++ strings/the-feast-of-many-beasts/solution.go
@@ -3,5 +3,8 @@
 package the_feast_of_many_beasts
 
 func Feast(beast string, dish string) bool {
-	// your code here
+	if beast[0] == dish[0] && beast[len(beast)-1] == dish[len(dish)-1] {
+		return true
+	}
+	return false
 }
