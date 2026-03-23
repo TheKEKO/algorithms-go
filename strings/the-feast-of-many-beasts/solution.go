793161c9e3cf722759892f8db1faec53b7d3d7ec
TheKEKO <a7214370@gmail.com>
2026-03-23 22:01:12 +0300

    wip: start The Feast of Many Beasts problem

---
+++ strings/the-feast-of-many-beasts/solution.go
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5aa736a455f906981800360d/train/go
+// The Feast of Many Beasts
+package the_feast_of_many_beasts
+
+func Feast(beast string, dish string) bool {
+	// your code here
+}
