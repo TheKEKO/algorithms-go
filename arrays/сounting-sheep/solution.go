5f178a2adee35b462ec6cf1d2a1fc61a7a5e1b1c
thekeko <a7214370@gmail.com>
2026-03-27 19:38:14 +0300

    wip: start Counting sheep... problem

---
+++ arrays/сounting-sheep/solution.go
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/54edbc7200b811e956000556/train/go
+// Counting sheep...
+
+package сounting_sheep
+
+func CountSheeps(numbers []bool) int {
+	return 0 // your code here
+}
