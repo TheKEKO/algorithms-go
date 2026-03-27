0e95de8c4107409fe39d93db38526d222bdaf9f7
thekeko <a7214370@gmail.com>
2026-03-27 17:30:32 +0300

    wip: strat Convert number to reversed array of digits problem

---
+++ arrays/convert-number-to-reversed-array-of-digits/solution.go
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5583090cbe83f4fd8c000051/train/go
+// Convert number to reversed array of digits
+
+package convert_number_to_reversed_array_of_digits
+
+func Digitize(n int) []int {
+	// your code here
+	return nil
+}
