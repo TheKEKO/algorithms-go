707227b9306745d79a958f31042331b0b71b3a70
thekeko <a7214370@gmail.com>
2026-03-29 00:29:23 +0300

    wip: start Simple multiplication problem

---
+++ math/simple-multiplication/solution.go
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/583710ccaa6717322c000105/train/go
+// Simple multiplication
+
+package simple_multiplication
+
+func SimpleMultiplication(n int) int {
+	return n // your code here
+}
