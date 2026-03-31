ab918e791a1fb1dd708dc1cf9494cf8d5f7d3e16
thekeko <a7214370@gmail.com>
2026-03-31 21:38:57 +0300

    wip: start Merge two sorted arrays into one problem

---
+++ arrays/merge-two-sorted-arrays-into-one/solution.go
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5899642f6e1b25935d000161/train/go
+// Merge two sorted arrays into one
+package merge_two_sorted_arrays_into_one
+
+func MergeArrays(arr1, arr2 []int) []int {
+	return []int{}
+}
