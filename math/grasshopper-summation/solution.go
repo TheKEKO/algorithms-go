be0b2e5640690c58973a9975e76d96e7a3570170
TheKEKO <a7214370@gmail.com>
2026-04-01 13:52:20 +0300

    wip: start Grasshopper - Summation problem

---
+++ math/grasshopper-summation/solution.go
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/go
+// Grasshopper - Summation
+package grasshopper_summation
+
+func Summation(n int) int {
+	// the sleeper must awaken!
+}
