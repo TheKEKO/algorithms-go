634c8b079843d446f35d2687c66828c0ee559d9a
TheKEKO <a7214370@gmail.com>
2026-04-01 13:42:31 +0300

    feat: add Merge two sorted arrays into one solution (arrays)

--- arrays/merge-two-sorted-arrays-into-one/solution.go
+++ arrays/merge-two-sorted-arrays-into-one/solution.go
@@ -2,6 +2,24 @@
 // Merge two sorted arrays into one
 package merge_two_sorted_arrays_into_one
 
+import "sort"
+
 func MergeArrays(arr1, arr2 []int) []int {
-	return []int{}
+	merged := append(arr1, arr2...)
+	sort.Ints(merged)
+
+	if len(merged) == 0 {
+		return merged
+	}
+
+	var res []int
+	unique := make(map[int]bool)
+
+	for _, v := range merged {
+		if !unique[v] {
+			unique[v] = true
+			res = append(res, v)
+		}
+	}
+	return res
 }
