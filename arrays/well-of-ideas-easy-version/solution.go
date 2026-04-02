6084a013543736175b7585630d1d2867990a68ac
TheKEKO <a7214370@gmail.com>
2026-04-02 18:17:22 +0300

    wip: start Well of Ideas - Easy Version problem

---
+++ arrays/well-of-ideas-easy-version/solution.go
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57f222ce69e09c3630000212/train/go
+// Well of Ideas - Easy Version
+package well_of_ideas_easy_version
+
+func Well(x []string) string {
+	// Your solution!
+}
