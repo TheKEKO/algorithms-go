920fcbe45fc25a8b470a83f684127d4f8d447c13
TheKEKO <a7214370@gmail.com>
2026-04-02 17:46:29 +0300

    wip: start Find the smallest integer in the array problem

---
+++ arrays/find-the-smallest-integer-in-the-array/solution.go
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/go
+// Find the smallest integer in the array
+package find_the_smallest_integer_in_the_array
+
+func SmallestIntegerFinder(numbers []int) int {
+	return 0 // your code here
+}
