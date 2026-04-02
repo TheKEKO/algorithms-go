cdceec97830beb4986e4e7ae71305f6296b8ad1b
TheKEKO <a7214370@gmail.com>
2026-04-02 18:52:13 +0300

    feat: add Well of Ideas - Easy Version solution (arrays)

--- arrays/well-of-ideas-easy-version/solution.go
+++ arrays/well-of-ideas-easy-version/solution.go
@@ -3,5 +3,19 @@
 package well_of_ideas_easy_version
 
 func Well(x []string) string {
-	// Your solution!
+	good := 0
+
+	for _, i := range x {
+		if i == "good" {
+			good++
+		}
+	}
+
+	if good == 0 {
+		return "Fail!"
+	} else if good <= 2 {
+		return "Publish!"
+	}
+
+	return "I smell a series!"
 }
