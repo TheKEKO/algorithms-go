0a2d0e10b59c7b52adbdad0c1e7e7c0cc989d8b6
thekeko <a7214370@gmail.com>
2026-04-04 23:15:12 +0300

    feat: add Volume of a Cuboid solution (math)

---
+++ math/volume-of-a-cuboid/solution.go
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/go
+// Volume of a Cuboid
+
+package volume_of_a_cuboid
+
+func GetVolumeOfCuboid(length, width, height float64) float64 {
+	return length * width * height
+}
