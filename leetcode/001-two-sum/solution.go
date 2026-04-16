b2acca9bfa10e0d18e4b00d7e5c6310a204017f5
TheKEKO <a7214370@gmail.com>
2026-04-16 21:16:29 +0300

    feat: add 1. Two Sum solution (leetcode)

---
+++ leetcode/001-two-sum/solution.go
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/two-sum/description/
+// 1. Two Sum
+package _01_two_sum
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] == target-nums[i] {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
