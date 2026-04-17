3f47237d0198d9c24d497e98f541ce1ea172a0f7
TheKEKO <a7214370@gmail.com>
2026-04-17 17:16:51 +0300

    wip: start 2. Add Two Numbers problem

---
+++ leetcode/002-add-two-numbers/solution.go
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/add-two-numbers/description/
+// 2. Add Two Numbers
+package _02_add_two_numbers
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+}
